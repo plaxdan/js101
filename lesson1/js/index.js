24cb48f1f26744be90ceab33c9baeb0b6a5d889a
Daniel Walker <plaxdan@gmail.com>
2015-10-24 19:16:40 -0600

    prepare js scratch pads

--- lesson1/js/index.js
+++ lesson1/js/index.js
@@ -1,22 +1,2 @@
 // This is a comment. Uncomment the code below to make it run!
-var age = 16;
-var under16 = age < 16;
-
-if (under16) {
-  console.warn("You cannot drive");
-} else if (age === 16) {
-  console.log("FINALLY I can drive!");
-} else {
-  console.log("Been drivin a while");
-}
-
-// PSEUDOCODE
-// if kettle has no water
-//   fill kettle
-// otherwise
-//   turn it on
-//   when its ready
-//     if cup on counter
-//       pour water
-//     else
-//       get cup out of cupboard
+// alert('Your code is running');
--- lesson2/js/index.js
+++ lesson2/js/index.js
@@ -1,10 +1,2 @@
 // This is a comment. Uncomment the code below to make it run!
-var city0 = 'Denver';
-var city1 = 'Tallahassee';
-var city2 = 'Zurich';
-
-// city1 "Tallahassee"
-// city2 "Zurich"
-
-var cities = ["Denver", "Tallahassee", "Zurich"];
-console.log(typeof cities);
+// alert('Your code is running');
